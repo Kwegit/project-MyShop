30968aed98252d848db259055bac4face35fbce5
Kwegit <axel.bou@epitech.digital>
2023-11-15 11:13:32 +0100

    base project

---
+++ vue-Myshop/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Home page content goes here.</p>
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ vue-Myshop/src/views/Product.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Product page content goes here.</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
