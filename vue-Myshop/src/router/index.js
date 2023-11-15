3a0308b4688f3bb6e6f8ca5d33ce3b2e49bdd2cd
noahdevv <noah.leonet-plancy@epitech.digital>
2023-11-15 16:13:57 +0100

    package-lock.json
    rajout du front admin (pas connecté à la database, que du visuel pour le moment) et la de la route /admin,

--- vue-Myshop/src/router/index.js
+++ vue-Myshop/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue';
-
+import Admin from '../components/Admin.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +16,13 @@ const router = createRouter({
       name: "product",
       component: Product,
     },
+
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin
+    },
+    
   ],
 });
 
